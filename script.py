497483655a8eda78de5888a956ccf94df098089c
Vinesh Raghvani <vineshraghvani1@gmail.com>
2024-03-06 02:59:58 -0700

    Add CSV

--- script.py
+++ script.py
@@ -1,10 +1,9 @@
-import seaborn 
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import linear_model
 
-df = pd.read_csv("https://content.codecademy.com/programs/data-science-path/linear_regression/honeyproduction.csv")
+df = pd.read_csv("honeyproduction.csv")
 
 #print(df.head())
 
